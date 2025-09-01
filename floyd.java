26cd7c5a7a986ff9dbbe48ce6008d51261153068
K Kevin Gladson <joeljoyson2427@gmail.com>
2025-09-02 05:11:53 +0530

    Add Floyd-Warshall algorithm implementation and clean up kahana.java

---
+++ floyd.java
@@ -0,0 +1,30 @@
+
+
+class Solution{
+     void floydWarshall(int n , int mat[][]){
+         int INF = (int)1e9;
+        int[][] dist = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    dist[i][j] = 0;
+                } else {
+                    dist[i][j] = INF;
+                }
+            }
+        }
+           for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] < INF && dist[k][j] < INF) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+    }
+}
+   
+       
+        
+        
